71c4c681f4bb917b816cad03518cd10e90fbdf14
Devon Neill <dev@neillfam.co.za>
2022-05-01 19:59:05 +0200

    Create Contact and Music pages

---
+++ src/pages/Music.tsx
@@ -0,0 +1,22 @@
+export function Music() {
+  return (
+    <>
+      <article className="flex flex-col w-full pt-10  gap-6">
+        <h1 className="text-3xl xl:text-5xl font-bold text-pink-600">Music</h1>
+        <p>Coming soon...</p>
+        {/* <section className="w-full p-10 border rounded-md border-pink-600">
+          <h2 className="text-3xl xl:text-5xl font-bold text-pink-600">
+            Overview
+          </h2>
+          Summary of music
+        </section>
+        <section className="w-full p-10 border rounded-md border-pink-600">
+          <h2 className="text-3xl xl:text-5xl font-bold text-pink-600">
+            Releases
+          </h2>
+          Spotify / soundcloud embeds
+        </section> */}
+      </article>
+    </>
+  );
+}
